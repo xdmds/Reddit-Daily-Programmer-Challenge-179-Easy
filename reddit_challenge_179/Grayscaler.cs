373f3c2b5c90ad88d3622aafa84d2d4951f5d803
xdmds <xdmds200092@gmail.com>
2014-09-12 15:23:26 -0400

    Solution completed

---
+++ reddit_challenge_179/Grayscaler.cs
@@ -0,0 +1,81 @@
+﻿/*
+ * Author: Derek Leung
+ * Description: Encapsulates all necessary functions to Manipulate the image in 
+ *              order to convert it to grayscale
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace reddit_challenge_179
+{
+    class Grayscaler
+    {
+        private Bitmap bmp;
+
+        /*
+         * Creates a Grayscaler object
+         * 
+         * Parameters: 
+         *  bmp (Bitmap) - the image to be converted to grayscale  
+         */ 
+        public Grayscaler(Bitmap bmp)
+        {
+            this.bmp = bmp;
+        }
+
+        /*
+         * Converts this Grayscaler object's image to grayscale by iterating
+         * through each pixel and changing its RGB value.
+         * 
+         */
+        public void ConvertGrayScale(){
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    Color px_color = bmp.GetPixel(x, y);
+                    int px_converted;
+                    px_converted= Formula(px_color);
+                    Color new_color = Color.FromArgb(px_converted, px_converted, px_converted);        
+                    bmp.SetPixel(x, y, new_color );
+                }
+            }
+        }
+
+        /*
+         * Converts the pixel RGB value based on a formula
+         * 
+         * Parameters:
+         *  px_color (Color) - the original pixel color
+         * 
+         * Return:
+         *  px_converted (int) - the converted pixel color rgb values
+         */ 
+        public int Formula(Color px_color){
+            int px_converted;
+
+            int r_val = px_color.R;
+            int g_val = px_color.G;
+            int b_val = px_color.B;
+
+
+            px_converted = (r_val + g_val + b_val) / 3;
+
+            return px_converted;
+        }
+
+        /*
+         * returns the image
+         */ 
+        public Bitmap getImage()
+        {
+            return this.bmp;
+        }
+    }
+}
---
+++ reddit_challenge_179/Program.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * Author: Derek Leung
+ * Description: http://www.reddit.com/r/dailyprogrammer/comments/2ftcb8/9082014_challenge_179_easy_you_make_me_happy_when/
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Collections;
+using System.Drawing;
+
+namespace reddit_challenge_179
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(File.Exists("//psf/Home/Desktop/1 - Motorcycles.png"));
+
+            String input = "";
+
+            Console.WriteLine("Please enter the valid path to the file you wish to convert to grayscale: ");
+            input = Console.ReadLine();
+
+            while (!File.Exists(input)){
+                Console.WriteLine("The path you entered does not exist.\n");
+                Console.WriteLine("Please enter the valid path to the file you wish to convert to grayscale: ");
+                input = Console.ReadLine();
+            }
+
+            //Directory exists
+            Bitmap bmp = new Bitmap(input);
+            
+            Grayscaler gs = new Grayscaler(bmp);
+
+            gs.ConvertGrayScale();
+            bmp = gs.getImage();
+
+            int i = input.LastIndexOf(".");
+            if (i > 0)
+            {
+                input = input.Substring(0, i);
+            }
+            bmp.Save(input + "-grayscale.png");
+            Console.Write("\nGrayscale conversion complete.\n\n The converted image has been saved to the following path: ");
+            Console.WriteLine(input);
+
+            
+            //prompt the user to close the program 
+            Console.Write("\nPress Enter to exit the program.");
+            Console.Read();
+           
+        }
+    }
+}
